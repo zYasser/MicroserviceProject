2faf8479352f316d66a11fd7570c07b78acc2125
yasserqamhiah <yaserqamhiah@gmail.com>
2023-02-21 23:30:05 +0300

    add oauth 2 to api gateway and secure it

---
+++ api-gateway/src/main/java/com.programming.gateway/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.programming.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity.csrf().disable().authorizeExchange(exchange -> {
+            exchange.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated();
+        }).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return httpSecurity.build();
+
+    }
+}
---
+++ discovery-server/src/main/java/com/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Value("${eureka.username}")
+    private String username;
+    @Value("${eureka.password}")
+    private String password;
+
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser(username).password(passwordEncoder().encode(password)).authorities("USER");
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().anyRequest().authenticated().and().httpBasic();
+        return http.build();
+    }
+}
